b75cd26ae56902ffcc15b8b9ffac40385f619588
vladimirdjurdjevic <vladimirdjurdjevic93@gmail.com>
2016-05-31 14:09:23 +0200

    Node server initial config

---
+++ pinfweb/pinfweb-angular-client/server.js
@@ -0,0 +1,20 @@
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+
+// configuration ===========================================
+var port = process.env.PORT || 8080; 
+
+app.use(bodyParser.json()); 
+
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static(__dirname + '/public')); 
+
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
+                   
+console.log('Server running on port: ' + port);
+        
+exports = module.exports = app;                   
