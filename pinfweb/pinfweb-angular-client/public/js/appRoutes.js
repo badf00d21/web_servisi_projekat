24fea318079594c0ad150e5b8b8349c20fe1acf2
vladimirdjurdjevic <vladimirdjurdjevic93@gmail.com>
2016-05-31 15:58:37 +0200

    Initial angular setup

---
+++ pinfweb/pinfweb-angular-client/public/js/appRoutes.js
@@ -0,0 +1,15 @@
+angular.module('appRoutes', []).config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider
+
+        .when('/url1', {
+            templateUrl: '',
+            controller: ''
+        })
+
+        .when('/url2', {
+            templateUrl: '',
+            controller: ''
+        });
+
+}]);
